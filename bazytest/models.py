123d650f2efe3417a9cc49ed39a49dbe7a3a0ca8
Alek <achrabas@student.agh.edu.pl>
2014-04-04 22:03:39 +0200

    Correct commit

--- bazytest/models.py
+++ bazytest/models.py
@@ -3,11 +3,20 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-  name = models.CharField(max_length=30)
-  join_date = models.DateTimeField()
+  login = models.CharField(max_length=30)
+  city = models.CharField(max_length=30)
+  create_date = models.DateTimeField()
+  age = models.IntegerField()
+  is_woman = models.BooleanField()
+
+class Thread(models.Model):
+  title = models.CharField(max_length=30)
+  create_date = models.DateTimeField()
+  user = models.ForeignKey(User)
 
 class Post(models.Model):
-  title = models.CharField(max_length=30)
+  thread = models.ForeignKey(Thread)
   content = models.TextField()
-  author = models.ForeignKey(User)
   date = models.DateTimeField()
+  author = models.ForeignKey(User)
+  location = models.CharField(max_length=30)
